6ccfdeff96885bd86f0a5b4a5e41cef9e4fcca3a
daniel <dgpb27@gmail.com>
2020-12-16 15:56:51 -0500

    media root

--- jobarts/settings.py
+++ jobarts/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/images/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, '/imagenes/')
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'static')
 #]
