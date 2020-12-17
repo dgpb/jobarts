05deace578da18831af30b6828df247e067b3d36
daniel <dgpb27@gmail.com>
2020-12-16 20:11:06 -0500

    static files root

--- jobarts/settings.py
+++ jobarts/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+#MEDIA_URL = '/images/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, '/images')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
