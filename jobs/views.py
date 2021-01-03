a05ca390a528d847fd1a2773dabb264f2f1f4631
daniel <dgpb27@gmail.com>
2021-01-03 12:27:16 -0500

    testing title

--- jobs/views.py
+++ jobs/views.py
@@ -27,22 +27,22 @@ def jobs(request):
 
     else:
         title = request.POST.get('title')
-        category = request.POST.get('category')
+        #category = request.POST.get('category')
 
         jobs = Job.objects.filter(job_title__icontains=title)
 
-        if category:
-            jobs = jobs.filter(job_type=category)
+        #if category:
+            #jobs = jobs.filter(job_type=category)
 
-            paginator = Paginator(jobs, 10)
-            page = request.GET.get('page')
+        paginator = Paginator(jobs, 10)
+        page = request.GET.get('page')
 
-            try:
-                jobs = paginator.page(page)
-            except PageNotAnInteger:
-                jobs = paginator.page(1)
-            except EmptyPage:
-                jobs = paginator.page(paginator.num_pages)
+        try:
+            jobs = paginator.page(page)
+        except PageNotAnInteger:
+            jobs = paginator.page(1)
+        except EmptyPage:
+            jobs = paginator.page(paginator.num_pages)
 
 
     return render(request, 'jobs/jobs.html', {'page' : page, 'jobs' : jobs})
