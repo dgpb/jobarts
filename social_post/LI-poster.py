c8ef54173457e90db706e407c9d21acb1013b57d
daniel <dgpb27@gmail.com>
2021-02-13 21:02:39 -0500

    social poster

---
+++ social_post/LI-poster.py
@@ -0,0 +1,45 @@
+import json
+import requests
+
+
+organization_id = '70892901'
+
+def read_creds(filename):
+    with open(filename) as f:
+        credentials = json.load(f)
+    return credentials
+
+credentials = read_creds('Li-creds.json')
+
+#scope: w_member_social,r_liteprofile,w_organization_social
+access_token = credentials['access_token']
+url = "https://api.linkedin.com/v2/ugcPosts"
+
+headers = {'Content-Type': 'application/json',
+           'X-Restli-Protocol-Version': '2.0.0',
+           'Authorization': 'Bearer ' + access_token}
+
+
+with open('10221jobarts.json') as f:
+    data = json.load(f)
+
+
+post_data = {
+    "author": "urn:li:organization:"+organization_id,
+    "lifecycleState": "PUBLISHED",
+    "specificContent": {
+        "com.linkedin.ugc.ShareContent": {
+            "shareCommentary": {
+                "text": "📢📢 Importante empresa esta buscando un " + data[0]['job_title'] + " en " + data[0]['city'] + " para una plaza " + data[0]['job_type'] + " . " + "No esperes más y POSTULA en el siguiente enlace 👉 " + data[0]['job_url'] + " 💼👍👍" + " #empleos #oportunidad #oportunidadlaboral #chamba #chambaparacreativos #jobarts"
+            },
+            "shareMediaCategory": "NONE"
+        }
+    },
+    "visibility": {
+        "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
+    }
+}
+
+response = requests.post(url, headers=headers, json=post_data)
+
+print(response)
---
+++ social_post/social-poster.py
@@ -0,0 +1,51 @@
+import facebook
+import json
+
+#Read Twitter credentials
+from twython import Twython
+from auth import (
+    api_key,
+    api_secret_key,
+    Access_token,
+    Access_token_secret
+)
+
+twitter = Twython(
+    api_key,
+    api_secret_key,
+    Access_token,
+    Access_token_secret
+)
+
+#Read Facebook credentials
+def read_creds(filename):
+    with open(filename) as f:
+        credentials = json.load(f)
+    return credentials
+
+credentials = read_creds('credentials.json')
+
+page_access_token = credentials['page_access_token']
+graph = facebook.GraphAPI(page_access_token)
+facebook_page_id = "101308201907030"
+
+
+#Build Facebook Post
+with open('10221jobarts.json') as f:
+    data = json.load(f)
+    graph.put_object(facebook_page_id, "feed", message="📢📢 Importante empresa esta buscando un " + data[0]['job_title'] + " en " + data[0]['city'] + " para una plaza " + data[0]['job_type'] + " . " + "No esperes más y POSTULA en 👉 " + data[0]['job_url'] + " 💼👍👍" + " #empleos #oportunidad #oportunidadlaboral #chamba #chambaparacreativos #jobarts", link=data[0]['job_url'])
+
+#Build Tweet
+with open('10221jobarts.json') as f:
+    data = json.load(f)
+
+    message = "📢📢 Importante empresa esta buscando un " + data[0]['job_title'] + " en " + data[0]['city'] + " para una plaza " + data[0]['job_type'] + ". " + "No esperes más y POSTULA en 👉 " + data[0]['job_url'] + " 💼👍👍" + " #empleos #oportunidad #oportunidadlaboral #chamba #chambaparacreativos #jobarts"
+    twitter.update_status(status=message)
+    data = data[1:]
+
+#Update json
+with open('10221jobarts.json', 'w') as handle:
+    handle.write(json.dumps(data, indent=4))
+
+    print("Posted job on Facebook")
+    print("Tweeted job on Tweeter")
