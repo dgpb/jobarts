a2db31a0e9cc11787628d2f85833e998966a12ba
daniel <dgpb27@gmail.com>
2021-06-01 20:18:44 -0500

    urls 4

--- yt_downloader/views.py
+++ yt_downloader/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def main(request):
-    return render(request, 'yt_downloader/ytb_main.html')
+def ytb(request):
+    return render(request, 'yt_downloader/ytb.html')
